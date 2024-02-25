b408c27a740abf05342b91fa4634a943279bb712
Alvaro Leiva Geisse <aleivag@meta.com>
2024-02-25 10:44:35 -0800

    updating podman

--- workshop/JOURNAL_101/journal_print.py
+++ workshop/JOURNAL_101/journal_print.py
@@ -4,7 +4,7 @@ import logging
 import pystemd.journal
 
 pystemd.journal.sendv(
-  f"PRIORITY={logging.ERROR}",
-  MESSAGE="Hello from pystemd journal printing",
-  SYSLOG_IDENTIFIER="superuniqueid"
+    f"PRIORITY={logging.ERROR}",
+    MESSAGE="Hello from pystemd journal printing",
+    SYSLOG_IDENTIFIER="superuniqueid",
 )
