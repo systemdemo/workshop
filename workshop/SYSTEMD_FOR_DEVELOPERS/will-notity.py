d76e4f3936a8d6b47bf094729d429be1cd664090
Alvaro Leiva <aleivag@gmail.com>
2024-03-15 08:45:55 -0700

    notify

--- workshop/SYSTEMD_FOR_DEVELOPERS/will-notity.py
+++ workshop/SYSTEMD_FOR_DEVELOPERS/will-notity.py
@@ -6,8 +6,5 @@ import pystemd.daemon
 print("I will notify")
 pystemd.daemon.notify(False, ready=1)
 
-pystemd.daemon.notify(False, watchdog=1, WATCHDOG_USEC=)
-
-
-time.sleep(3.5)
+time.sleep(3)
 print("Will this line be reach?")
